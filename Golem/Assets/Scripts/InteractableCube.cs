f1e4a56b69039b1e93db87e215eb6afe2c1f4f2c
dyfust <dyfust@gmail.com>
2020-08-13 14:59:40 +1000

    Interactable cube fix & material fix

--- Golem/Assets/Scripts/InteractableCube.cs
+++ Golem/Assets/Scripts/InteractableCube.cs
@@ -3,32 +3,29 @@
 public class InteractableCube : MonoBehaviour
 {
 	[SerializeField] private float _mass; public float mass => _mass;
-	[SerializeField] private float _rayPadding = 0; 
+	[SerializeField] private float _rayPadding = 0;
+	[SerializeField] private LayerMask _groundLayer;
 	private FixedJoint _fj;
 	private Rigidbody _rb;
 
 	private bool _isInteracted = false; 
 
-	private int _layermask = 1 << 9; 
-
-	// Start is called before the first frame update
 	void Start()
 	{
 		_fj = GetComponent<FixedJoint>();
 		_rb = GetComponent<Rigidbody>();
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 
 		if (_isInteracted == false)
 		{
 			float halfLength = transform.localScale.x / 2;
-			bool _topRight = Physics.Raycast(transform.position + new Vector3(halfLength, -halfLength + 0.1f, halfLength), Vector3.down, _rayPadding, _layermask);
-			bool _bottomRight = Physics.Raycast(transform.position + new Vector3(halfLength, -halfLength + 0.1f, -halfLength), Vector3.down, _rayPadding, _layermask);
-			bool _bottomLeft = Physics.Raycast(transform.position + new Vector3(-halfLength, -halfLength + 0.1f, -halfLength), Vector3.down, _rayPadding, _layermask);
-			bool _topLeft = Physics.Raycast(transform.position + new Vector3(-halfLength, -halfLength + 0.1f, halfLength), Vector3.down, _rayPadding, _layermask);
+			bool _topRight = Physics.Raycast(transform.position + new Vector3(halfLength, -halfLength + 0.1f, halfLength), Vector3.down, _rayPadding, _groundLayer);
+			bool _bottomRight = Physics.Raycast(transform.position + new Vector3(halfLength, -halfLength + 0.1f, -halfLength), Vector3.down, _rayPadding, _groundLayer);
+			bool _bottomLeft = Physics.Raycast(transform.position + new Vector3(-halfLength, -halfLength + 0.1f, -halfLength), Vector3.down, _rayPadding, _groundLayer);
+			bool _topLeft = Physics.Raycast(transform.position + new Vector3(-halfLength, -halfLength + 0.1f, halfLength), Vector3.down, _rayPadding, _groundLayer);
 
 			if (!_topRight && !_topLeft && !_bottomRight && !_bottomLeft)
 			{
@@ -38,7 +35,7 @@ public class InteractableCube : MonoBehaviour
 					_rb.isKinematic = false;
 			}
 
-			if (_topRight && _topLeft && _bottomRight && _bottomLeft)
+			if (_topRight || _topLeft || _bottomRight || _bottomLeft)
 			{
 				if (_fj == null)
 					_fj = gameObject.AddComponent<FixedJoint>();
@@ -46,7 +43,6 @@ public class InteractableCube : MonoBehaviour
 					_rb.isKinematic = true;
 			}
 		}
-
 	}
 
 	public void SetIsInteractable(bool state)
