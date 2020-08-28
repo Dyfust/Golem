3f3dc2511ec10b59f8fda02e8d6c6e3dedd09e7b
dyfust <dyfust@gmail.com>
2020-08-28 12:42:42 +1000

    setting up vscode

--- Golem/Assets/Scripts/BoulderSpawner.cs
+++ Golem/Assets/Scripts/BoulderSpawner.cs
@@ -14,12 +14,6 @@ public class BoulderSpawner : MonoBehaviour
 
     private float _timer = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
