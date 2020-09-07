2aa3fe4a6794f1b42ec275382a364dfcd633e480
dyfust <dyfust@gmail.com>
2020-09-07 10:09:57 +1000

    Character Controller refinements

--- Golem/Assets/Scripts/Movement/IMovementController.cs
+++ Golem/Assets/Scripts/Movement/IMovementController.cs
@@ -4,8 +4,6 @@ public interface IMovementController
 {
     void FixedUpdate();
     void Move(Vector3 dir);
-    void AddForce(Vector3 force);
-    void AddExternalForce(Vector3 force);
     void OnCollisionStay(Collision collision);
     bool IsGrounded();
 }
