c899f8e3cf368fc70bd9b932610ce9c394eb0137
dyfust <dyfust@gmail.com>
2020-09-25 00:45:40 +1000

    Improved File Structure & Input system

---
+++ Golem/Assets/Scripts/Reset System/ResetSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ResetSystem : MonoBehaviour
+{
+	[SerializeField] private ResetBoundingBox _startingBoundingBox; 
+
+	private static ResetSystem _instance;
+	public static ResetSystem instance => _instance;
+
+	private const string _identifierTag = "Room"; 
+
+	private ResetBoundingBox[] _boundingBoxes;
+	private ResetBoundingBox _currentBoundingBox;
+
+	private void Awake()
+	{
+		if (_instance == null)
+			_instance = this;
+		else
+			Debug.LogWarning("Multiple reset systems present!");
+
+		GameObject[] boundingBoxGOs = GameObject.FindGameObjectsWithTag(_identifierTag);
+		_boundingBoxes = new ResetBoundingBox[boundingBoxGOs.Length];
+
+		for (int i = 0; i < _boundingBoxes.Length; i++)
+		{
+			_boundingBoxes[i] = boundingBoxGOs[i].GetComponent<ResetBoundingBox>(); 
+		}
+
+		_currentBoundingBox = _startingBoundingBox; 
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			_currentBoundingBox.Reset(); 
+		}
+	}
+
+	public void SetCurrentBoundingBox(ResetBoundingBox currentBoundingBox) => _currentBoundingBox = currentBoundingBox;
+}
