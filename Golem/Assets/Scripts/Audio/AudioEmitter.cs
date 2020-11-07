d849a9ca3b1f8d1131d8cfeb0c2545e8128d4333
Dyfust <dyfust@gmail.com>
2020-11-07 21:19:26 +1100

    Tidied up dragon platform in main room & reduced volume of golem platform

--- Golem/Assets/Scripts/Audio/AudioEmitter.cs
+++ Golem/Assets/Scripts/Audio/AudioEmitter.cs
@@ -4,10 +4,12 @@ public class AudioEmitter : MonoBehaviour
 {
     [SerializeField] private AudioSource _source;
     [SerializeField] private AudioClip _clip;
+    [SerializeField] private float _maxVolume = 1f;
     [SerializeField] private float _fadeInDuration = 1f;
     [SerializeField] private float _fadeOutDuration = 1f;
 
     private int _fade; // 0 = fade out, 1 = fade in
+    private float _targetVolume;
     private float _fadeSpeed;
     private bool _fading = false;
 
@@ -22,7 +24,7 @@ public class AudioEmitter : MonoBehaviour
     {
         if (_fading)
         {
-            SetVolume(Mathf.MoveTowards(_source.volume, _fade, _fadeSpeed * Time.deltaTime));
+            SetVolume(Mathf.MoveTowards(_source.volume, _targetVolume, _fadeSpeed * Time.deltaTime));
 
             if (_source.volume == _fade)
                 _fading = false;
@@ -56,9 +58,10 @@ public class AudioEmitter : MonoBehaviour
     private void Fade(int fade)
     {
         _fade = Mathf.Clamp(fade, 0, 1);
+        _targetVolume = _maxVolume * _fade;
 
         float time = fade == 1 ? _fadeInDuration : _fadeOutDuration;
-        _fadeSpeed = Mathf.Abs(fade - _source.volume) / time;
+        _fadeSpeed = Mathf.Abs(_targetVolume - _source.volume) / time;
         _fading = true;
     }
 
