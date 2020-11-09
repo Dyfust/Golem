493c978f9703bab62e5322a7e05977a63fd5ba7c
dyfust <dyfust@gmail.com>
2020-11-10 02:28:53 +1100

    FINAL ADJUSTMENTS TO AUDIO, SCENE AND MENUS

---
+++ Golem/Assets/Scripts/UI/SliderComp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderComp : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private SliderValue _value;
+
+    private void Start()
+    {
+        _slider.value = Mathf.InverseLerp(_value.min, _value.max, _value.value);
+    }
+
+    public void OnValueChanged()
+    {
+        _value.value = Mathf.Lerp(_value.min, _value.max, _slider.value);
+    }
+}
+
+[CreateAssetMenu(fileName = "Slider Value", menuName = "Values/Slider Value", order = 0)]
+public class SliderValue : ScriptableObject
+{
+    public float value;
+    public float min;
+    public float max;
+}
