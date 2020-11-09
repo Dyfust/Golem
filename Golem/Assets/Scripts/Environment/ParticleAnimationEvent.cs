207b2dc0f5e4b0960398ebfb68b7ed456c672121
dyfust <dyfust@gmail.com>
2020-11-09 15:18:39 +1100

    Flower bloom particle effect

---
+++ Golem/Assets/Scripts/Environment/ParticleAnimationEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ParticleAnimationEvent : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particleEffect;
+
+    public void PlayParticle()
+    {
+        _particleEffect.Play();
+    }
+}
